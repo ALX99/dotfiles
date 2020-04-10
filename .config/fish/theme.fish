7596bebff3acb37264943307e6771e4f4bdb0e67
ALX99 <46844683+ALX99@users.noreply.github.com>
2020-04-10 14:39:00 +0200

    (curl http://whatthecommit.com/index.txt)

---
+++ .config/fish/theme.fish
@@ -0,0 +1,26 @@
+set -U fish_color_normal normal
+set -U fish_color_command green
+set -U fish_color_quote 29c4c4
+set -U fish_color_redirection e4e861
+set -U fish_color_end FF7B7B
+set -U fish_color_error d12c2c
+set -U fish_color_param f05d78
+set -U fish_color_comment 30BE30
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd 73c9a5
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 777777
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -b $fish_color_error
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+
