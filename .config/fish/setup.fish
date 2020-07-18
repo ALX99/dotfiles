392f157dda191670f844b4e55939a29af9862606
ALX99 <46844683+ALX99@users.noreply.github.com>
2020-07-18 17:12:36 +0200

    update

--- .config/fish/setup.fish
+++ .config/fish/setup.fish
@@ -4,7 +4,7 @@ echo "Setting up fish..."
 rm -rf ~/.config/fish/fish_variables
 
 # Set environtment variables
-set -Ua fish_user_paths $HOME/.local/bin
+set -Ua fish_user_paths (du "$HOME/.local/bin/" | cut -f2 | paste -sd':')
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 #set -U FILE pcmanfm
@@ -20,6 +20,8 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+fisher
+fisher self-update
 
 if command -vq gh
     echo "Github CLI found!"
