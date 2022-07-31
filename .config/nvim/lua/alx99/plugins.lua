2b65763bc38c671e3a765931fe61b0b16a7c9ebc
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-07-31 16:52:21 +0200

    Configure nvim when used with vscode

--- .config/nvim/lua/alx99/plugins.lua
+++ .config/nvim/lua/alx99/plugins.lua
@@ -1,3 +1,7 @@
+if vim.g.vscode then
+  return -- Currently we have nothing we want to configure when using vscode
+end
+
 -- :help nvim-tree-setup
 -- :help nvim-tree.OPTION_NAME
 require("nvim-tree").setup({
