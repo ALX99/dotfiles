3cb9378751df6375e7ef6da1c4979993c32b3bf4
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-07-31 18:11:32 +0200

    Add neovim languageservers

--- .config/nvim/lua/alx99/plugins.lua
+++ .config/nvim/lua/alx99/plugins.lua
@@ -9,6 +9,24 @@ view = {
     adaptive_size = true,
   },
 })
-
 vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>NvimTreeFindFile<CR>', { noremap = true })
 
+
+-- Install gopls
+require("lspconfig").gopls.setup{}
+-- Install shellcheck and https://github.com/bash-lsp/bash-language-server
+require("lspconfig").bashls.setup{}
+
+-- Install https://github.com/rcjsuen/dockerfile-language-server-nodejs (used by the vscode docker extension)
+require'lspconfig'.dockerls.setup{}
+
+-- Install https://github.com/redhat-developer/yaml-language-server
+require('lspconfig').yamlls.setup{
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+      },
+    },
+  }
+}
