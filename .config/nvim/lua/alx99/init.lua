e84403e11172ff821e2d7ee7b4e62540e937a588
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-08-20 08:54:11 +0200

    create new neovim plugin directory

--- .config/nvim/lua/alx99/init.lua
+++ .config/nvim/lua/alx99/init.lua
@@ -1,16 +1,6 @@
-require('alx99.packer')
+-- require('alx99.packer')
 
 require('alx99.set')
-require('alx99.plugins.comment')
-if (not vim.g.vscode) then
-  require('alx99.plugins.notify') -- Notifications
-  require('alx99.plugins.toggleterm') -- Better terminal
-  require('alx99.plugins.autopairs') -- Autopairs
-  require('alx99.plugins.treesitter') -- Generate ASTs
-  require('alx99.plugins.cmp') -- Completion
-  require('alx99.plugins.gitsigns') -- Gitgutter
-  require('alx99.plugins.nvimtree') -- Filetree
-  require('alx99.plugins.lsp') -- LSP
-end
+require('plugins')
 require('alx99.keymap')
 require('alx99.autocmds')
