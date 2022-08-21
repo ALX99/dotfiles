ad7ee01aa721d55a4176fc3b6d486308ad806ddc
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-08-21 21:02:44 +0200

    cleanup

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,6 @@
 local plugins = {
   ['wbthomason/packer.nvim'] = {}, -- Packer can mange itself
 
-
   -- Functionality
   ['junegunn/vim-easy-align'] = {},
   ['numToStr/Comment.nvim'] = {
@@ -14,8 +13,8 @@ local plugins = {
 
   -- Treesitter is able to generate ASTs for almost all languages
   ['nvim-treesitter/nvim-treesitter'] = {
-    config = function() require('plugins.configs.treesitter') end,
     disable = vim.g.vscode,
+    config = function() require('plugins.configs.treesitter') end,
   },
 
   -- Colorschemes
@@ -42,27 +41,29 @@ local plugins = {
     end,
   },
 
-
-
+  -- Eyecandy for notifications
   ['rcarriga/nvim-notify'] = {
     disable = vim.g.vscode,
     config = function() require('plugins.configs.notify') end,
-  }, -- Eyecandy for notifications
+  },
 
-
-  -- LSP & IDE features
+  -- Autopairs
   ['windwp/nvim-autopairs'] = {
+    disable = vim.g.vscode,
     config = function() require('plugins.configs.autopairs') end,
+  },
+
+  -- Snippet engine, configured with cmp
+  ['L3MON4D3/LuaSnip'] = {
     disable = vim.g.vscode,
   },
 
-  -- Completion
-  ['L3MON4D3/LuaSnip'] = {
-    disable = vim.g.vscode,
-  }, -- Snippet engine, configured with cmp
+  -- Snippet engine support for cmp, configured with cmp
   ['saadparwaiz1/cmp_luasnip'] = {
     disable = vim.g.vscode,
-  }, -- Snippet engine support for cmp, configured with cmp
+  },
+
+  -- Autocompletion
   ['hrsh7th/nvim-cmp'] = {
     disable = vim.g.vscode,
     config = function() require('plugins.configs.cmp') end,
@@ -80,16 +81,19 @@ local plugins = {
     requires = { 'hrsh7th/cmp-nvim-lsp', 'ray-x/lsp_signature.nvim' },
   },
 
+  -- Tree file manager
   ['kyazdani42/nvim-tree.lua'] = {
     disable = vim.g.vscode,
     config = function() require('plugins.configs.nvimtree') end,
-  }, -- Tree file manager
+  },
 
+  -- Git gutters and blames
   ['lewis6991/gitsigns.nvim'] = {
     disable = vim.g.vscode,
     config = function() require('plugins.configs.gitsigns') end,
-  }, -- Git gutters and blames
+  },
 
+  -- Fuzzy finder that does it all
   ['nvim-telescope/telescope.nvim'] = {
     disable = vim.g.vscode,
     tag = '0.1.0',
