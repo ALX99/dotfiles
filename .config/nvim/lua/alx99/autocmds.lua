9d12364bf239c1d1723f1f85aad423944646d9e4
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-08-21 18:18:13 +0200

    run shfmt when saving shellscripts

--- .config/nvim/lua/alx99/autocmds.lua
+++ .config/nvim/lua/alx99/autocmds.lua
@@ -1,64 +1,79 @@
 -- show cursor line only in active window
 local cursorGrp = vim.api.nvim_create_augroup("CursorLine", { clear = true })
-vim.api.nvim_create_autocmd(
-  { "InsertLeave", "WinEnter" },
-  { pattern = "*", command = "set cursorline", group = cursorGrp }
-)
-vim.api.nvim_create_autocmd(
-  { "InsertEnter", "WinLeave" },
-  { pattern = "*", command = "set nocursorline", group = cursorGrp }
-)
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  group = cursorGrp,
+  pattern = "*",
+  command = "set cursorline",
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+  pattern = "*",
+  command = "set nocursorline",
+  group = cursorGrp
+})
 
 -- return to last edit position when opening files
-local bufcheck = vim.api.nvim_create_augroup('bufcheck', { clear = true })
-vim.api.nvim_create_autocmd(
-  'BufReadPost',
-  { group = bufcheck,
-    pattern  = '*',
-    callback = function()
-      if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-        vim.fn.setpos('.', vim.fn.getpos("'\""))
-        vim.api.nvim_feedkeys('zz', 'n', true)
-      end
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group    = vim.api.nvim_create_augroup('ReturnToPos', { clear = true }),
+  pattern  = '*',
+  callback = function()
+    if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.fn.setpos('.', vim.fn.getpos("'\""))
+      vim.api.nvim_feedkeys('zz', 'n', true)
     end
-  })
+  end
+})
 
 -- https://github.com/neovim/nvim-lspconfig/issues/115
 -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
-local preBufCheck = vim.api.nvim_create_augroup('prebufcheck', { clear = true })
-vim.api.nvim_create_autocmd(
-  'BufWritePre',
-  { group = preBufCheck,
-    pattern  = '*.go',
-    callback = function()
-      local wait_ms = 1000
-      local params = vim.lsp.util.make_range_params()
-      params.context = { only = { "source.organizeImports" } }
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-      for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-          else
-            vim.lsp.buf.execute_command(r.command)
-          end
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group    = vim.api.nvim_create_augroup('GoOrganizeImports', { clear = true }),
+  pattern  = '*.go',
+  callback = function()
+    local wait_ms = 1000
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
         end
       end
     end
-  })
-
--- Highlight on yank
-local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+  end
 })
 
--- local bufEnter = vim.api.nvim_create_augroup("BufEnter", { clear = true })
--- -- Don't auto commenting new lines
--- -- https://github.com/NvChad/NvChad/blob/main/lua/core/init.lua
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = "*",
---   command = "set fo-=c fo-=r fo-=o",
---   group = bufEnter,
--- })
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  command = "silent! lua vim.highlight.on_yank()",
+})
+
+
+-- Make sure :filetype is on
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("autoformat", { clear = true }),
+  callback = function(info)
+    if vim.o.filetype == "sh" then
+      vim.cmd(":w") -- Can't be bothered to figure out the piping yet
+      local job = vim.fn.jobstart({ "shfmt", info.file }, {
+        stdout_buffered = true,
+        stderr_buffered = true,
+        on_stdout = function(_, data)
+          if data then
+            vim.api.nvim_buf_set_lines(info.buf, 0, -1, true, data)
+          end
+        end,
+        on_stderr = function(_, data)
+          if data and data[1] == '' then
+            return
+          end
+          vim.notify("shfmt failed", vim.log.levels.ERROR)
+        end
+      })
+      vim.fn.jobwait({ job })
+    end
+  end
+})
