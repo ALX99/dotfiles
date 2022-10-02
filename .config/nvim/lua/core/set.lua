654b1bc2fd85e2fbc59d653997335932ef432636
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-10-02 14:59:14 +0200

    remove gui stuff

--- .config/nvim/lua/core/set.lua
+++ .config/nvim/lua/core/set.lua
@@ -101,34 +101,6 @@ for _, plugin in pairs(default_plugins) do
   g["loaded_" .. plugin] = 1
 end
 
-g.gui_font_default_size = 8
-g.gui_font_size = vim.g.gui_font_default_size
-g.gui_font_face = "Fira Code Retina"
-
-RefreshGuiFont = function()
-  vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
-end
-
-ResizeGuiFont = function(delta)
-  vim.g.gui_font_size = vim.g.gui_font_size + delta
-  RefreshGuiFont()
-end
-
-ResetGuiFont = function()
-  vim.g.gui_font_size = vim.g.gui_font_default_size
-  RefreshGuiFont()
-end
-
--- Call function on startup to set default value
-ResetGuiFont()
-
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set({ 'n', 'i' }, "<C-+>", function() ResizeGuiFont(1) end, opts)
-vim.keymap.set({ 'n', 'i' }, "<C-->", function() ResizeGuiFont(-1) end, opts)
-vim.keymap.set({ 'n', 'i' }, "<C-BS>", function() ResetGuiFont() end, opts)
-
-
 -- TODO stuff left over from general.vim
 --au! BufWritePost $MYVIMRC source %      " auto source when writing to init.vm alternatively you can run :source $MYVIMRC
 -- set breakindent                         -- Wrap lines at same indent level
