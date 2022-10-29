1d1b4b9c386675ed2781f3c61b524da708d13c67
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-10-29 09:33:00 +0200

    add lspkind

--- .config/nvim/lua/plugins/configs/cmp.lua
+++ .config/nvim/lua/plugins/configs/cmp.lua
@@ -3,6 +3,7 @@ local utils = require('core.utils')
 
 local cmp = utils.require('cmp')
 local luasnip = utils.require('luasnip')
+local lspkind = utils.require('lspkind')
 local vscode_snips = utils.require('luasnip.loaders.from_vscode')
 if not (cmp and luasnip and vscode_snips) then return end
 
@@ -17,6 +18,13 @@ local has_words_before = function()
 end
 
 cmp.setup({
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+    }),
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
