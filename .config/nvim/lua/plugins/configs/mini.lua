de69934fa179d1972cb838b63e60cc8d074e3ca0
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-10-29 09:18:05 +0200

    add tidy.nvim and remove vim.g.vscode

--- .config/nvim/lua/plugins/configs/mini.lua
+++ .config/nvim/lua/plugins/configs/mini.lua
@@ -4,7 +4,7 @@ local trailspace = utils.require('mini.trailspace')
 if not trailspace then return end
 trailspace.setup({})
 utils.require('mini.tabline').setup({})
-utils.require('mini.pairs').setup({})
+-- utils.require('mini.pairs').setup({})
 utils.require('mini.cursorword').setup({})
 utils.require('mini.statusline').setup({})
 utils.require('mini.align').setup({})
