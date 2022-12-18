27fdda6efe0fba3653cc5aae3369b3df76e29ff9
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-12-18 13:09:26 +0100

    remove commit locking

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ local plugins = {
 
   -- Functionality
   ['ggandor/leap.nvim'] = {
-    commit = 'c19e974cfd9d52dc5070ec7b68183da39702c877'
   },
   ['alx99/qc.lua'] = {
     config = function()
@@ -22,7 +21,6 @@ local plugins = {
 
   ['akinsho/toggleterm.nvim'] = {
     config = function() require('plugins.configs.toggleterm') end,
-    commit = '3ba683827c623affb4d9aa518e97b34db2623093'
   },
 
   -- Treesitter is able to generate ASTs for almost all languages
@@ -35,7 +33,6 @@ local plugins = {
 
   -- Colorschemes
   ['folke/tokyonight.nvim'] = {
-    commit = 'ec144d94a8dbd9c8b4f5e50d933d67a37f589ed8'
   },
   ['EdenEast/nightfox.nvim'] = {
     config = function()
@@ -46,7 +43,6 @@ local plugins = {
       })
       vim.cmd("colorscheme carbonfox")
     end,
-    commit = 'e70c3edabf30c671230371ff941df63ef82ee441'
   },
   --   ['navarasu/onedark.nvim'] = {
   --     disable = true,
@@ -73,24 +69,21 @@ local plugins = {
 
   -- Snippet engine, configured with cmp
   ['L3MON4D3/LuaSnip'] = {
-    commit = '619796e2477f7233e5fdff456240676a08482684'
   },
 
   -- Snippet engine support for cmp, configured with cmp
   ['saadparwaiz1/cmp_luasnip'] = {
-    commmit = '18095520391186d634a0045dacaa346291096566'
   },
 
   -- Autocompletion
   ['hrsh7th/nvim-cmp'] = {
     config = function() require('plugins.configs.cmp') end,
     requires = {
-      { 'hrsh7th/cmp-buffer', commit = '3022dbc9166796b644a841a02de8dd1cc1d311fa' },
-      { 'saadparwaiz1/cmp_luasnip', commit = '18095520391186d634a0045dacaa346291096566' },
-      { 'L3MON4D3/LuaSnip', commit = '619796e2477f7233e5fdff456240676a08482684' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'L3MON4D3/LuaSnip' },
       { 'alx99/lspkind.nvim' }
     },
-    commmit = 'c53dd36adcf512611fa7c523fced55447bfdbfa5'
   },
 
   -- LSP setup
@@ -111,31 +104,29 @@ local plugins = {
   -- Tree file manager
   ['kyazdani42/nvim-tree.lua'] = {
     config = function() require('plugins.configs.nvimtree') end,
-    commit = 'e204a7d819a9a065d5b1cdc6f59d2d2777d14a0f'
   },
 
   -- Git gutters and blames
   ['lewis6991/gitsigns.nvim'] = {
     config = function() require('plugins.configs.gitsigns') end,
-    commit = '9ff7dfb051e5104088ff80556203634fc8f8546d'
   },
 
   -- Fuzzy finder that does it all
   ['nvim-telescope/telescope.nvim'] = {
     config = function() require('plugins.configs.telescope') end,
     tag = '0.1.0',
-    requires = { 'nvim-lua/plenary.nvim', commit = '4b7e52044bbb84242158d977a50c4cbcd85070c7' }
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+    }
   },
 
   -- Code actions for telescope
   ['nvim-telescope/telescope-ui-select.nvim'] = {
-    commit = '62ea5e58c7bbe191297b983a9e7e89420f581369'
   },
 
   -- Utilities
   ['echasnovski/mini.nvim'] = {
     config = function() require('plugins.configs.mini') end,
-    commit = '9061584513afd62ad6a08757b0aacf5d656bdf36'
   },
 }
 
