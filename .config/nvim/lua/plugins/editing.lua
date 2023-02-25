b878d79852d42659ab325f81c1de7a597eb93e2d
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-02-25 09:48:00 +0100

    add harpoon

---
+++ .config/nvim/lua/plugins/editing.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      { "1",         "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "Harpoon 1" },
+      { "2",         "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "Harpoon 2" },
+      { "3",         "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "Harpoon 3" },
+      { "4",         "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",         desc = "Harpoon 4" },
+      { "<leader>h", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Harpoon add file" },
+      { "<leader>H", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon UI" },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+}
