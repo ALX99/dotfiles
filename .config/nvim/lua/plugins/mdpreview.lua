078a254938f16457619f06156bcc2764b2316257
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-02-25 03:04:07 +0100

    only enable markdown-preview when yarn is installed

--- .config/nvim/lua/plugins/mdpreview.lua
+++ .config/nvim/lua/plugins/mdpreview.lua
@@ -1,5 +1,6 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+  enabled = vim.fn.executable("yarn") == 1,
   build = "cd app && yarn install",
 }
