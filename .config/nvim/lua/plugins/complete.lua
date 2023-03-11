1f048a35d5d6e00fafa2f5c09ac4559318ad79b0
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-03-12 00:19:01 +0100

    update cmp config

--- .config/nvim/lua/plugins/complete.lua
+++ .config/nvim/lua/plugins/complete.lua
@@ -10,19 +10,16 @@ return {
 
   -- completion
   {
-
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
       'hrsh7th/cmp-buffer',
       'saadparwaiz1/cmp_luasnip',
       'L3MON4D3/LuaSnip',
-      'onsails/lspkind.nvim',
     },
-    config = function()
+    opts = function(_, opts)
       local cmp = require('cmp')
       local luasnip = require('luasnip')
-      local lspkind = require('lspkind')
 
 
       -- Helper function
@@ -31,74 +28,81 @@ return {
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      cmp.setup({
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol', -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-          }),
-        },
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<A-e>'] = cmp.mapping.scroll_docs( -4),
-          ['<A-n>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<Tab>"] = cmp.mapping(function(fallback) -- Prefer to jump if possible
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif cmp.visible() then
-              cmp.select_next_item()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback) -- Prefer to jump if possible
-            if luasnip.jumpable( -1) then
-              luasnip.jump( -1)
-            elseif cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        }, {
-          { name = 'buffer' },
-        })
+      opts.mapping = cmp.mapping.preset.insert({
+        -- confirm completion
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- cancel completion
+        ['<Esc>'] = cmp.mapping.abort(),
+        -- scroll up and down in the completion documentation
+        ['<A-e>'] = cmp.mapping.scroll_docs(-4),
+        ['<A-n>'] = cmp.mapping.scroll_docs(4),
+        ["<Tab>"] = cmp.mapping(function(fallback) -- Prefer to jump if possible
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback) -- Prefer to jump if possible
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          elseif cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       })
+
+      opts.formatting = {
+        fields = { 'abbr', 'menu', 'kind' },
+        format = function(entry, item)
+          local short_name = {
+            nvim_lsp = 'LSP',
+            nvim_lua = 'nvim'
+          }
+          local menu_name = short_name[entry.source.name] or entry.source.name
+          item.menu = string.format('[%s]', menu_name)
+          return item
+        end,
+      }
+
+      opts.snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      }
+
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+
+
+      opts.sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+      })
+    end,
+    config = function(_, opts)
+      local cmp = require('cmp')
+
+      cmp.setup(opts)
 
       -- Set configuration for specific filetype.
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources({
-          { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-        }, {
-          { name = 'buffer' },
-        })
-      })
-
-      -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline('/', {
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
           { name = 'buffer' }
         }
       })
-    end,
+    end
   }
 }
