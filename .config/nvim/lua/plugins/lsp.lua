90b2fb3573844a92eb9387e1d21fd111392d451c
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-05-28 00:42:41 +0200

    wow semantic tokens

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -99,6 +99,23 @@ return {
           vim.bo[ev.buf].formatexpr = "v:lua.vim.lsp.formatexpr()"
         end
 
+        -- workaround for gopls not supporting semantictokensprovider
+        -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+        if client.name == "gopls" then
+          if not client.server_capabilities.semanticTokensProvider then
+            local semantic = client.config.capabilities.textDocument.semanticTokens
+            client.server_capabilities.semanticTokensProvider = {
+              full = true,
+              legend = {
+                tokenTypes = semantic.tokenTypes,
+                tokenModifiers = semantic.tokenModifiers,
+              },
+              range = true,
+            }
+          end
+        end
+        -- end workaround
+
         mappings(ev.buf)
         highlight_references(client, ev.buf)
         formatting(client, ev.buf)
@@ -171,11 +188,11 @@ return {
       settings = {
         gopls = {
           experimentalPostfixCompletions = true,
+          semanticTokens = true,
           -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
           analyses = {
             unusedparams = true,
             shadow = true,
-            fieldalignment = false,
             nilness = true,
             unusedwrite = true,
             useany = true
