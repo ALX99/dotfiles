1a66c05470db264572fc2cbe14fe8c5a7a0bbf66
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-10-21 14:52:31 +0900

    feat: improve luasnip integration
    now it is possible to use <Tab> and <S-Tab> to jump between snippet places

--- .config/nvim/lua/plugins/complete.lua
+++ .config/nvim/lua/plugins/complete.lua
@@ -22,6 +22,7 @@ return {
     },
     opts = function(_, _)
       local cmp = require('cmp')
+      local luasnip = require("luasnip")
 
       return {
         snippet = {
@@ -56,10 +57,29 @@ return {
               fallback()
             end
           end,
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources(
           {
-            { name = "nvim_lsp" }, { name = "luasnip" },
+            { name = "luasnip" }, { name = "nvim_lsp" }
           },
           {
             { name = "buffer" },
