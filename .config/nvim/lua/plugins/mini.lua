9576e29fa4f56b65cdd3e7cd53862588d781a844
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-10-21 20:36:42 +0900

    chore: fmt

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -59,7 +59,12 @@ return {
       'nvim-tree/nvim-web-devicons'
     },
     keys = {
-      { "<leader>ft", ":lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>", mode = { "n", "x", "o" }, desc = "MiniFiles" },
+      {
+        "<leader>ft",
+        ":lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>",
+        mode = { "n", "x", "o" },
+        desc = "MiniFiles"
+      },
     },
     opts = {
       mappings = {
