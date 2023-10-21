7f3b97d4600d46ddccf67360cd89c86643c8cacb
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-10-21 20:26:35 +0900

    feat: autocreate the sessions directory
    in case the nvim sessions directory does not exists
    it will now be created automatically

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -7,8 +7,15 @@ local function get_session_file()
   if vim.fn.has("win32") == 1 then
     pattern = "[\\:]"
   end
-  return vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/")
-      .. vim.fn.getcwd():gsub(pattern, "%%") .. ".vim"
+
+  local dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions")
+
+  -- Create the dir if it doesn't exists
+  if vim.fn.isdirectory(dir) == 0 then
+    vim.cmd("!mkdir -p " .. vim.fn.fnameescape(dir))
+  end
+
+  return dir .. "/" .. vim.fn.getcwd():gsub(pattern, "%%") .. ".vim"
 end
 
 -- Check if we need to reload the file when it changed
@@ -65,9 +72,9 @@ local sessionGrp = vim.api.nvim_create_augroup("auto_sessions", { clear = true }
 vim.api.nvim_create_autocmd("VimEnter", {
   desc = "Restores the previous session",
   callback = function()
-    local session = get_session_file()
-    if vim.fn.argc() == 0 and vim.fn.filereadable(session) ~= 0 then
-      vim.cmd("silent! source " .. vim.fn.fnameescape(session))
+    local session_file = get_session_file()
+    if vim.fn.argc() == 0 and vim.fn.filereadable(session_file) ~= 0 then
+      vim.cmd("silent! source " .. vim.fn.fnameescape(session_file))
     end
     return true -- Delete the autocmd (only needs to run once)
   end,
