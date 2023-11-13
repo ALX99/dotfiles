bfd503c000201a13e4b64559154a8df7ae0a6407
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-11-13 23:13:04 +0900

    feat: use make instead of cmake

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,8 +7,7 @@ return {
     -- Fuzzy finder
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      build =
-      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      build = 'make'
     }
   },
   cmd = "Telescope",
