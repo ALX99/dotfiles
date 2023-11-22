b10d82f51b82b429e5b7c0a9ec8b975f305c777d
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-11-23 00:56:36 +0900

    refactor: create session plugin

---
+++ .config/nvim/lua/custom_plugins/session.lua
@@ -0,0 +1,47 @@
+local M = {
+  dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions")
+}
+
+function M.setup()
+  -- Create the dir if it doesn't exists
+  if vim.fn.isdirectory(M.dir) == 0 then
+    vim.cmd("!mkdir -p " .. vim.fn.fnameescape(M.dir))
+  end
+
+  local session_group = vim.api.nvim_create_augroup("auto_sessions", { clear = true })
+
+  vim.api.nvim_create_autocmd("VimEnter", {
+    desc = "Restores the previous session",
+    callback = function()
+      local session_file = M.get_session_file()
+      if vim.fn.argc() == 0 and vim.fn.filereadable(session_file) ~= 0 then
+        vim.cmd("silent! source " .. vim.fn.fnameescape(session_file))
+      end
+      return true -- Delete the autocmd (only needs to run once)
+    end,
+    group = session_group,
+    nested = true,
+  })
+
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    desc = "Saves the session",
+    callback = function()
+      if vim.fn.argc() == 0 and vim.fn.getcwd() ~= vim.env.HOME then
+        vim.cmd("mks! " .. vim.fn.fnameescape(M.get_session_file()))
+      end
+      return true -- Delete the autocmd (only needs to run once)
+    end,
+    group = session_group
+  })
+end
+
+function M.get_session_file()
+  local pattern = "/"
+  if vim.fn.has("win32") == 1 then
+    pattern = "[\\:]"
+  end
+
+  return M.dir .. "/" .. vim.fn.getcwd():gsub(pattern, "%%") .. ".vim"
+end
+
+return M
