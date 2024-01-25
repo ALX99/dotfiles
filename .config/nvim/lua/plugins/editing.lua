9d5ee7028cb8951c56a5841cee1d7ce42e20cf52
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-01-25 23:12:42 +0900

    perf: copilot.vim is VeryLazy

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -83,7 +83,7 @@ return {
   },
   {
     "github/copilot.vim",
-    event = { "InsertEnter" },
+    event = { "VeryLazy" },
     config = function()
       -- vim.g.copilot_assume_mapped = true
       vim.g.copilot_no_tab_map = true
