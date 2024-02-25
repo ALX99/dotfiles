c5f16499b718dcccbae9200e92894574141345ae
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-02-25 12:44:28 +0900

    no emoji

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -87,12 +87,12 @@ o.helpheight  = 25
 
 
 -- Diagnostics
--- Setup some nicer icons for diagnostics in the gutter
-local signs = { Error = "󰅚", Warn = "", Hint = "󰛩", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+-- Setup some nicer icons for diagnostics in the gutter (emoji)
+-- local signs = { Error = "󰅚", Warn = "", Hint = "󰛩", Info = " " }
+-- for type, icon in pairs(signs) do
+--   local hl = "DiagnosticSign" .. type
+--   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+-- end
 
 vim.diagnostic.config({
   virtual_text = true,
