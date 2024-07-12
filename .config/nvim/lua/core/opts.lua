7686bdd138a1e46f45d43a6b68b76f7b8d020f30
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-07-13 01:34:43 +0900

    feat: better vim.ui.open

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -117,3 +117,25 @@ vim.diagnostic.config({
 --   vim.lsp.handlers.signature_help,
 --   { focusable = true, style = "minimal", border = "rounded" }
 -- )
+
+vim.ui.open = (function(overridden)
+  return function(path)
+    vim.validate({
+      path = { path, 'string' },
+    })
+    local is_uri = path:match('%w+:')
+    local is_half_url = path:match('%.com$')
+    local is_repo = vim.bo.filetype == 'lua' and path:match('%w/%w') and vim.fn.count(path, '/') == 1
+    local is_dir = path:match('/%w')
+    if not is_uri then
+      if is_half_url then
+        path = ('https://%s'):format(path)
+      elseif is_repo then
+        path = ('https://github.com/%s'):format(path)
+      elseif not is_dir then
+        path = ('https://google.com/search?q=%s'):format(path)
+      end
+    end
+    overridden(path)
+  end
+end)(vim.ui.open)
