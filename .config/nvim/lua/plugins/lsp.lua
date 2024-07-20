6aa0fcdc9bb62a5631cc2c1ef4f250d2d2e1c175
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-07-21 04:48:00 +0900

    lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,127 +1,147 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = { "hrsh7th/cmp-nvim-lsp" },
-  config = function()
-    local lspconfig = require('lspconfig')
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local lspconfig = require('lspconfig')
 
-    -- Set up
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-
-    -- configure the capabilities to be passed to all LSPs
-    lspconfig.util.default_config = vim.tbl_extend(
-      "force",
-      lspconfig.util.default_config,
-      { capabilities = capabilities }
-    )
+      -- Set up
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
 
-    -----------------
-    -- LSP Servers --
-    -----------------
-    -- https://github.com/neovim/nvim-lspconfig
-    local lsps = {
-      clangd = {},
-      pyright = {},
-      dockerls = {},
-      rust_analyzer = {},
-      jsonls = {},
-      html = {},
-      cssls = {},
-      sourcekit = {},
-      tailwindcss = {},
-      tsserver = {},
-      svelte = {},
-      robotframework_ls = {},
-      zls = {},
-      eslint = {},
-      gleam = {},
-      nim_langserver = {},
-      terraformls = {},
-      erlangls = {},
-      gopls = {
-        -- cmd = { vim.fn.getenv("HOME") .. "/go/bin/gopls" },
-        settings = {
-          gopls = {
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true
-            },
-            gofumpt = true,
-            staticcheck = true,
-            usePlaceholders = true,
-            semanticTokens = true,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+      -- configure the capabilities to be passed to all LSPs
+      lspconfig.util.default_config = vim.tbl_extend(
+        "force",
+        lspconfig.util.default_config,
+        { capabilities = capabilities }
+      )
 
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
-            },
-            -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
-            analyses = {
-              fieldalignment = true,
-              unusedvariable = true,
-              unusedparams = true,
-              unusedwrite = true,
-              nilness = true,
-              shadow = true,
-              useany = true,
-            },
-          },
-        },
-      },
-      -- https://github.com/redhat-developer/yaml-language-server
-      yamlls = {
-        settings = {
-          yaml = {
-            schemas = {
-              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+
+      -----------------
+      -- LSP Servers --
+      -----------------
+      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+      local lsps = {
+        clangd = {},
+        pyright = {},
+        dockerls = {},
+        rust_analyzer = {},
+        jsonls = {},
+        html = {},
+        cssls = {},
+        sourcekit = {},
+        tailwindcss = {},
+        tsserver = {},
+        svelte = {},
+        robotframework_ls = {},
+        zls = {},
+        eslint = {},
+        gleam = {},
+        nim_langserver = {},
+        terraformls = {},
+        erlangls = {},
+        gopls = {
+          -- cmd = { vim.fn.getenv("HOME") .. "/go/bin/gopls" },
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true
+              },
+              gofumpt = true,
+              staticcheck = true,
+              usePlaceholders = true,
+              semanticTokens = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+
+              codelenses = {
+                gc_details = true,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+              analyses = {
+                fieldalignment = true,
+                unusedvariable = true,
+                unusedparams = true,
+                unusedwrite = true,
+                nilness = true,
+                shadow = true,
+                useany = true,
+              },
             },
           },
         },
-      },
-      lua_ls = {
-        on_init = function(client)
-          local path = client.workspace_folders[1].name
-          if not vim.uv.fs_stat(path .. '/.luarc.json') and not vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-              Lua = {
-                runtime = {
-                  version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = vim.api.nvim_get_runtime_file("", true)
+        -- https://github.com/redhat-developer/yaml-language-server
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = {
+                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+              },
+            },
+          },
+        },
+        lua_ls = {
+          on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+              return
+            end
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME
                 }
+                -- This pulls in a lot or more files
+                -- library = vim.api.nvim_get_runtime_file("", true)
               }
             })
-
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-          end
-          return true
-        end
+          end,
+          settings = {
+            Lua = {
+              runtime = {
+                version = 'LuaJIT'
+              },
+            }
+          }
+        }
       }
 
-    }
-
-    for name, set in pairs(lsps) do
-      lspconfig[name].setup(set)
+      for name, set in pairs(lsps) do
+        lspconfig[name].setup(set)
+      end
+    end,
+    cond = function()
+      return not require('core.utils').is_vscodevim()
     end
-  end,
-  cond = function()
-    return not require('core.utils').is_vscodevim()
-  end
+  },
+  {
+    "folke/lazydev.nvim",
+    version      = "*",
+    ft           = "lua",
+    dependencies = { "Bilal2453/luvit-meta" },
+    opts         = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+    cond         = function()
+      return not require('core.utils').is_vscodevim()
+    end,
+    enabled      = require('core.utils').is_linux
+  },
+
 }
