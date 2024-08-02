ebaaa5fca9039cfcd77241c05d3747c120e85a62
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-08-02 22:24:47 +0900

    fill background color in terminal

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -136,3 +136,17 @@ local function update_lead()
 end
 autocmd("OptionSet", { pattern = { "listchars", "tabstop", "filetype" }, callback = update_lead })
 autocmd("VimEnter", { callback = update_lead, once = true })
+
+-- https://www.reddit.com/r/neovim/comments/1ehidxy/you_can_remove_padding_around_neovim_instance/
+-- wait for require("mini.misc").setup_termbg_sync() to be fixed
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
