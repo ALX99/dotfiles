4c2fa4b41a30012ef28f342cfe2c10e02dcc759d
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-08-14 22:18:30 +0900

    setup_restore_cursor()

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -60,15 +60,16 @@ autocmd({ "InsertEnter", "WinLeave" }, {
 })
 
 -- return to last edit position when opening files
-autocmd('BufReadPost', {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
+-- this is handled by require("mini.misc").setup_restore_cursor() now
+-- autocmd('BufReadPost', {
+--   callback = function()
+--     local mark = vim.api.nvim_buf_get_mark(0, '"')
+--     local lcount = vim.api.nvim_buf_line_count(0)
+--     if mark[1] > 0 and mark[1] <= lcount then
+--       pcall(vim.api.nvim_win_set_cursor, 0, mark)
+--     end
+--   end,
+-- })
 
 -- Make sure :filetype is on
 autocmd("BufWritePost", {
