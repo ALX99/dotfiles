db33baf327c3118bfbb4dc250ba9583fdab57e24
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-08-14 22:47:47 +0900

    better yamlls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -101,6 +101,13 @@ return {
               schemas = {
                 ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
               },
+              format = {
+                enable = true,
+                bracketSpacing = true
+              },
+              schemaStore = {
+                enable = true
+              }
             },
           },
         },
