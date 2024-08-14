4d2b7d1b557e65032903c49423685111c8a55d81
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-08-14 22:18:30 +0900

    setup_restore_cursor()

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,6 +6,7 @@ return {
       'nvim-tree/nvim-web-devicons'
     },
     config = function()
+      require("mini.misc").setup_restore_cursor()
       require("mini.notify").setup({})
       require("mini.ai").setup({
         mappings = {
