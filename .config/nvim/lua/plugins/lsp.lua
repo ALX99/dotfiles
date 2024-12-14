7154c0b2c7c13948ca02a1cee2ee58a2ee4d63b1
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-12-14 11:24:11 +0900

    fix: fieldalignment has been removed

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -84,7 +84,6 @@ return {
               },
               -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
               analyses = {
-                fieldalignment = true,
                 unusedvariable = true,
                 unusedparams = true,
                 unusedwrite = true,
