c2fc639f95bac36f15fdfd7a9a9c45ccd58a341c
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-10-02 09:52:26 +0200

    feat: auto format imports for go

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -95,6 +95,22 @@ autocmd("BufWritePost", {
   end
 })
 
+autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+        for _, res in pairs(result or {}) do
+            for _, action in pairs(res.result or {}) do
+                if action.edit then
+                    vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+                end
+            end
+        end
+    end,
+})
+
 -- autocmd("BufReadPost", {
 --   desc = "Collapse error handling with one line inside",
 --   group = augroup("go-fold", { clear = true }),
