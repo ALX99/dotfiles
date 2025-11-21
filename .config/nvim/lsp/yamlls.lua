7060cb57a72303bab7b0bf9bfe77a1405d86e608
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-11-21 22:38:57 +0900

    chore: fixes

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -3,8 +3,6 @@ return {
     yaml = {
       schemas = {
         ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-        ["/home/dozy/projects/ika/config/schema.json"] = "ika.yaml",
-        -- ["/home/dozy/projects/ika/config/schema.json"] = "ika.example.yaml",
       },
       format = {
         enable = true,
