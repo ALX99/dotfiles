13b7322b5d57da7e48d6f6690c8142da3b39ca78
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-11-29 12:06:18 +0900

    refactor(nvim): move gitgud keymaps to plugin spec
    Migrated <leader>Gl keymappings for Git permalinks from core/keymaps.lua to the gitgud plugin specification in plugins/editing.lua. This ensures keymaps are managed via lazy.nvim and associated directly with the plugin.

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -107,6 +107,30 @@ return {
     opts = {
       greeting = "Hello from gitgud",
     },
+    keys = {
+      {
+        "<leader>Gl",
+        function()
+          require("gitgud").copy_github_permalink()
+        end,
+        mode = "n",
+        desc = "Copy GitHub permalink",
+      },
+      {
+        "<leader>Gl",
+        function()
+          local start_line = vim.fn.line("v")
+          local end_line = vim.fn.line(".")
+          if end_line < start_line then
+            start_line, end_line = end_line, start_line
+          end
+          require("gitgud").copy_github_permalink({ start_line = start_line, end_line = end_line })
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
+        end,
+        mode = "x",
+        desc = "Copy GitHub permalink (range)",
+      },
+    },
   },
   {
     "windwp/nvim-ts-autotag",
