49c55b521e9aa916d0f6291463146854e5acb8dc
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-11-30 09:22:19 +0900

    feat: git stuff

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -93,60 +93,6 @@ return {
     end
   },
   {
-    dir = vim.fn.stdpath("config") .. "/lua/custom_plugins/gitgud",
-    name = "gitgud",
-    dev = true,
-    opts = {
-      greeting = "Hello from gitgud",
-    },
-    keys = {
-      {
-        "<leader>Gl",
-        function()
-          require("gitgud").copy_github_permalink()
-        end,
-        mode = "n",
-        desc = "Copy GitHub permalink",
-      },
-      {
-        "<leader>Gl",
-        function()
-          local start_line = vim.fn.line("v")
-          local end_line = vim.fn.line(".")
-          if end_line < start_line then
-            start_line, end_line = end_line, start_line
-          end
-          require("gitgud").copy_github_permalink({ start_line = start_line, end_line = end_line })
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
-        end,
-        mode = "x",
-        desc = "Copy GitHub permalink (range)",
-      },
-      {
-        "<leader>Go",
-        function()
-          require("gitgud").open_github_file()
-        end,
-        mode = "n",
-        desc = "Open GitHub file",
-      },
-      {
-        "<leader>Go",
-        function()
-          local start_line = vim.fn.line("v")
-          local end_line = vim.fn.line(".")
-          if end_line < start_line then
-            start_line, end_line = end_line, start_line
-          end
-          require("gitgud").open_github_file({ start_line = start_line, end_line = end_line })
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
-        end,
-        mode = "x",
-        desc = "Open GitHub file (range)",
-      },
-    },
-  },
-  {
     "windwp/nvim-ts-autotag",
     enabled = false,
     cond = function()
@@ -163,9 +109,5 @@ return {
     cond = function()
       return vim.g.vscode
     end,
-  },
-  {
-    "f-person/git-blame.nvim",
-    opts = {},
   },
 }
