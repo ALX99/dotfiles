851bdcacb2b3ea485d6be26a31378a056d6363c5
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-12-19 21:37:55 +0900

    refactor: treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     lazy = false,
-    branch = 'main',
     config = function(_, _)
-      local treesitter = require('nvim-treesitter')
-
-      local ensure_installed = {
+      require('nvim-treesitter').install({
         -- "c",
         -- "cpp",
         "bash",
@@ -18,9 +15,10 @@ return {
         "html",
         "javascript",
         "typescript",
+        "tsx",
         "json",
         "lua",
-        -- "luadoc",
+        "luadoc",
         "make",
         "markdown",
         "markdown_inline",
@@ -29,36 +27,41 @@ return {
         "yaml",
         "regex", -- for Snacks.picker
         "gitcommit",
-        "tsx"
-      }
+      })
 
-      local already_installed = treesitter.get_installed('parsers')
-      local parsers_to_install = vim.iter(ensure_installed)
-          :filter(function(parser) return not vim.tbl_contains(already_installed, parser) end)
-          :totable()
-      if #parsers_to_install > 0 then
-        treesitter.install(parsers_to_install)
+
+      if not vim.g.vscode then
+        return -- if in vscode, do nothing
       end
 
+      local group = vim.api.nvim_create_augroup('treesitter_filetypes', { clear = true })
 
-      -- Start treesitter highlighting if not in vscode
-      if not vim.g.vscode then
-        vim.api.nvim_create_autocmd('FileType', {
-          callback = function(args)
-            local filetype = args.match
-            local lang = vim.treesitter.language.get_lang(filetype)
-            if lang ~= nil and vim.treesitter.language.add(lang) then
-              -- syntax highlighting, provided by Neovim
-              vim.treesitter.start()
-              -- folds, provided by Neovim
-              vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-              vim.wo.foldmethod = "expr"
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        desc = 'Enable treesitter highlighting and indentation',
+        callback = function(event)
+          local lang = vim.treesitter.language.get_lang(event.match) or event.match
+          local buf = event.buf
+
+
+          if lang ~= nil and vim.treesitter.language.add(lang) then
+            -- syntax highlighting, provided by Neovim
+            pcall(vim.treesitter.start, buf, lang)
+
+
+            if vim.treesitter.query.get(lang, "indents") then
               -- indentation, provided by nvim-treesitter
               vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end
+
+            if vim.treesitter.query.get(lang, "folds") then
+              -- folds, provided by Neovim
+              vim.wo.foldmethod = "expr"
+              vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            end
           end
-        })
-      end
+        end
+      })
     end,
   },
   {
