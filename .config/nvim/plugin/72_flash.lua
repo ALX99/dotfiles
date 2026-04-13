35e7fbcc3f513ecbf91d0cdd67a0a277a02ef4e0
ALX99 <46844683+ALX99@users.noreply.github.com>
2026-04-13 21:16:31 +0900

    chore: flash

---
+++ .config/nvim/plugin/72_flash.lua
@@ -0,0 +1,25 @@
+-- flash.nvim (jump navigation) + vscode mode setup
+if vim.g.vscode then
+  require("custom_plugins.vscode").setup()
+  return
+end
+
+vim.pack.add({
+  'https://github.com/folke/flash.nvim',
+})
+
+local map = require('utils').map
+
+-- flash.nvim
+require("flash").setup({
+  labels = "arstgmneiofpludh",
+  modes = {
+    char = { enabled = false },
+  },
+})
+
+map({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+map({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+map("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+map({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Flash Treesitter Search" })
+map("c", "<a-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
