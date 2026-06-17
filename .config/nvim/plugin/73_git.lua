c946b4197ca5b876a0cb04a302ef183e3dd0ad3d
ALX99 <46844683+ALX99@users.noreply.github.com>
2026-06-17 21:12:04 +0900

    refactor(nvim): DRY visual-range handlers in 73_git.lua
    ponytail: <leader>Gl and <leader>Go had identical 6-line bodies that
    only differed in which gitgud function they called. Extract
    with_visual_range helper, pass the function in.

--- .config/nvim/plugin/73_git.lua
+++ .config/nvim/plugin/73_git.lua
@@ -8,23 +8,21 @@ vim.pack.add({
 local map = require('utils').map
 local gitgud = require('custom.gitgud')
 
+local function with_visual_range(callback)
+  return function()
+    local start_line = vim.fn.line("v")
+    local end_line = vim.fn.line(".")
+    if end_line < start_line then start_line, end_line = end_line, start_line end
+    callback({ start_line = start_line, end_line = end_line })
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
+  end
+end
+
 map('n', '<leader>Gl', function() gitgud.copy_github_permalink() end, { desc = "Copy GitHub permalink" })
-map('x', '<leader>Gl', function()
-  local start_line = vim.fn.line("v")
-  local end_line = vim.fn.line(".")
-  if end_line < start_line then start_line, end_line = end_line, start_line end
-  gitgud.copy_github_permalink({ start_line = start_line, end_line = end_line })
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
-end, { desc = "Copy GitHub permalink (range)" })
+map('x', '<leader>Gl', with_visual_range(gitgud.copy_github_permalink), { desc = "Copy GitHub permalink (range)" })
 
 map('n', '<leader>Go', function() gitgud.open_github_file() end, { desc = "Open GitHub file" })
-map('x', '<leader>Go', function()
-  local start_line = vim.fn.line("v")
-  local end_line = vim.fn.line(".")
-  if end_line < start_line then start_line, end_line = end_line, start_line end
-  gitgud.open_github_file({ start_line = start_line, end_line = end_line })
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
-end, { desc = "Open GitHub file (range)" })
+map('x', '<leader>Go', with_visual_range(gitgud.open_github_file), { desc = "Open GitHub file (range)" })
 
 -- blame.nvim
 require('blame').setup({
