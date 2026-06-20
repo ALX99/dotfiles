ba2f673bff6f0d3cacbf187d6de789861937a98d
ALX99 <46844683+ALX99@users.noreply.github.com>
2026-06-20 11:55:54 +0900

    chore(nvim): drop jsonls and yamlls lsp support
    Remove jsonls and yamlls from the enabled LSP list and drop
    schemastore.nvim, which only existed to provide their schemas.
    Also removes unused codediff.nvim and bumps kanagawa, markview,
    and mason to current revisions.

--- .config/nvim/plugin/78_lsp.lua
+++ .config/nvim/plugin/78_lsp.lua
@@ -4,7 +4,6 @@ if vim.g.vscode then return end
 
 vim.pack.add({
   { src = 'https://github.com/neovim/nvim-lspconfig', version = vim.version.range('*') },
-  'https://github.com/b0o/schemastore.nvim', -- used by jsonls/yamlls lsp configs
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -17,13 +16,13 @@ vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
   local client = ctx and ctx.client_id and vim.lsp.get_client_by_id(ctx.client_id)
   local prefix = client and ("[LSP] [" .. client.name .. "]") or "[LSP]"
   -- LSP MessageType: 1=Error, 2=Warning, 3=Info, 4=Log, 5=Debug (LSP 3.18+)
-  local level = ({ [1] = vim.log.levels.ERROR, [2] = vim.log.levels.WARN, [3] = vim.log.levels.INFO, [4] = vim.log.levels.DEBUG, [5] = vim.log.levels.DEBUG })[result.type] or vim.log.levels.INFO
+  local level = ({ [1] = vim.log.levels.ERROR, [2] = vim.log.levels.WARN, [3] = vim.log.levels.INFO, [4] = vim.log.levels.DEBUG, [5] = vim.log.levels.DEBUG })
+      [result.type] or vim.log.levels.INFO
   vim.notify(prefix .. " " .. result.message, level)
 end
 
 local enabled_lsps = {
   "pyright",
-  "jsonls",
   "html",
   "cssls",
   "tailwindcss",
@@ -31,7 +30,6 @@ local enabled_lsps = {
   "terraformls",
   "eslint",
   "gopls",
-  "yamlls",
   "lua_ls",
   "copilot",
   "gh_actions_ls",
